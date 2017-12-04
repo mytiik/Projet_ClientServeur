5b15265e7248ee660f6eacdab28e63807b20a829
DylanTds <dylan.tavares.de.sa@gmail.com>
2017-12-04 11:19:17 +0100

    ajout fichiers comServ

---
+++ src/comServCli/AdressServeurTcp.java
@@ -0,0 +1,12 @@
+
+package comServCli;
+
+public class AdressServeurTcp extends Object {
+        
+    public boolean equals(){
+        return true;
+    }
+    public int hashCode(){
+        return 0;
+    }
+}
