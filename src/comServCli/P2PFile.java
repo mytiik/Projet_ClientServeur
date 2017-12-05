0369bb66c864e018d16fd78e71b219ffd5ad48ed
DylanTds <dylan.tavares.de.sa@gmail.com>
2017-12-05 11:09:24 +0100

    F2PFile terminé

--- src/comServCli/P2PFile.java
+++ src/comServCli/P2PFile.java
@@ -1,30 +1,77 @@
 
 package comServCli;
-/*
-   definir equals
-   definir hashcode
-*/
+import java.util.Arrays;
+import java.util.Objects;
 
-public class P2PFile extends Object {
-    String name;
-    double size;
+
+public class P2PFile {
     
-    // Default constructor
-    public P2PFile( String name, double size ){
+    private String      name = "";
+    private int         size = 0;
+    private String[]    listeClient = null;
+    
+    // default constructor
+    public P2PFile( String name, int size ){
         this.name = name;
         this.size = size;
     }
-    // (name only)
+    // ^ (name only)
     public P2PFile( String name ){
         this.name = name;
-        this.size = 0;
+    }
+
+    // getters
+    public String[] getListeClient() {
+        return listeClient;
+    }
+    public String   getName() {
+        return name;
+    }
+    public double   getSize() {
+        return size;
     }
     
-    public boolean equals(){
-        return true;
+    // setters
+    public void     setListeClient(String[] listeClient) {
+        this.listeClient = listeClient;
     }
-    public int hashCode(){
-        return 0;
+    public void     setName(String name) {
+        this.name = name;
+    }
+    public void     setSize(int size) {
+        this.size = size;
+    }
+    
+    @Override
+    public int      hashCode(){
+        int result = 14;
+        
+        result = 31 * result + this.name.hashCode();
+        result = 31 * result + this.size;
+        result = 31 * result + Arrays.hashCode(this.listeClient);
+        
+        return result;
+    }
+
+    @Override
+    public boolean  equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final P2PFile other = (P2PFile) obj;
+        if (Double.doubleToLongBits(this.size) != Double.doubleToLongBits(other.size)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
     }
     
 }
